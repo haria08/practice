f97407e239b12f535de5c85b25a5fa96592943b1
haria08 <harinarayanno1@gmail.com>
2024-07-09 16:09:24 +0530

    sum wu kong

---
+++ 2.sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"enter num"<<a<<endl;
+cin>>a;
+cout<<"enter num"<<b<<endl;
+cin>>b;
+cout<<"sum of num"<<a+b<<endl;
+return 0;
+}
