be0db8306ba76427c918ecd387227cef19786e05
haria08 <harinarayanno1@gmail.com>
2024-07-21 12:43:34 +0530

    intializevariable

--- output/6.scope_resolution.cpp
+++ output/6.scope_resolution.cpp
@@ -1,21 +1,6 @@
-#include<iostream>
-using namespace std;
-int m = 10;
+#include <iostream>
 int main()
 {
- int m=  20;
- {
-    int k=m;
-    int m=30;
-
-    cout <<"we are in innner block\n";
-    cout<<"k="<<k<<"\n";
-    cout<<"m="<<m<<"\n";
-    cout<<"::m="<<::m<<"\n";
-
- }
- cout<<"\nWe are in outer block \n";
- cout<<"m ="<<m<<"\n";
- cout<<"::m = " << ::m <<"\n";
- return 0;
- }
+   std::cout << "Hello world!";
+   return 0;
+}
